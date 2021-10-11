8dbe3eca5f75f18564afe9251ec14a89b800ced8
ajhosan <alexjhosan19@gmail.com>
2021-10-11 15:08:24 +0700

    validasi password

--- controllers/api-detail-by-username.js
+++ controllers/api-detail-by-username.js
@@ -3,24 +3,37 @@ const posts = require('../data/login.json');
 const getByUsername = (req, res) => {
     // Insert Login Code Here
     let username = req.body.username;
+
+    let password = req.body.password;
   
     const post = posts.find((value) => value.username === username)
-  
-    if(post != undefined){
-      return res.status(200).json({
-        code:200,
-        message: 'Akun Berhasil Login',
-        data: post || null,
-        error: null
-    })
+
+    if(password === post.password){
+      if(post != undefined){
+        return res.status(200).json({
+          code:200,
+          message: 'Akun Berhasil Login',
+          data: post || null,
+          error: null
+      })
+      }else{
+        return res.status(404).json({
+          code:404,
+          message: 'Akun Not Found',
+          data: null,
+          error: null
+      })
+      }
     }else{
-      return res.status(404).json({
-        code:404,
-        message: 'Akun Not Found',
+      return res.status(500).json({
+        code:500,
+        message: 'Password tidak sama',
         data: null,
         error: null
     })
     }
+  
+    
     
   }
 
